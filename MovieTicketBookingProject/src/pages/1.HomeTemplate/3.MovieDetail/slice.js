852b888ae33b59c3484e4071edc66db446e089d1
diepphuphuocloc24 <diepphuphuocloc24@gmail.com>
2025-11-27 11:02:10 +0700

    Loc update List TimeShow for Detail

--- MovieTicketBookingProject/src/pages/1.HomeTemplate/3.MovieDetail/slice.js
+++ MovieTicketBookingProject/src/pages/1.HomeTemplate/3.MovieDetail/slice.js
@@ -4,11 +4,14 @@ import axios from "axios";
 const initialState = {
   loading: false,
   dataDetail: null,
+  dataCinemaList: null,
+  dataCinema: null,
+  dataTimeShow: null,
   error: null,
 };
 
 export const fetchMovieDetail = createAsyncThunk(
-  "movie-detail/fetchMovieDetail",
+  "movie/fetchMovieDetail",
   async (id, { rejectWithValue }) => {
     try {
       const result = await axios({
@@ -19,7 +22,6 @@ export const fetchMovieDetail = createAsyncThunk(
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA5MCIsIkhldEhhblN0cmluZyI6IjI5LzA1LzIwMjYiLCJIZXRIYW5UaW1lIjoiMTc4MDAxMjgwMDAwMCIsIm5iZiI6MTc1MzAzMDgwMCwiZXhwIjoxNzgwMTYwNDAwfQ.KkGRtLpEsgoM4M_TapjOZIzvAwbay3QvXIwwN8XUqWk",
         },
       });
-
       return result.data.content;
     } catch (error) {
       return rejectWithValue(error);
@@ -27,25 +29,120 @@ export const fetchMovieDetail = createAsyncThunk(
   }
 );
 
-const movieDetailSlice = createSlice({
-  name: "movieDetailSlice",
+export const fetchCinemaList = createAsyncThunk(
+  "movie/fetchMovieCinema",
+  async (_, { rejectWithValue }) => {
+    try {
+      const result = await axios({
+        url: "https://movienew.cybersoft.edu.vn/api/QuanLyRap/LayThongTinHeThongRap",
+        method: "GET",
+        headers: {
+          TokenCybersoft:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA5MCIsIkhldEhhblN0cmluZyI6IjI5LzA1LzIwMjYiLCJIZXRIYW5UaW1lIjoiMTc4MDAxMjgwMDAwMCIsIm5iZiI6MTc1MzAzMDgwMCwiZXhwIjoxNzgwMTYwNDAwfQ.KkGRtLpEsgoM4M_TapjOZIzvAwbay3QvXIwwN8XUqWk",
+        },
+      });
+      return result.data.content;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+export const fetchCinema = createAsyncThunk(
+  "movie/fetchCinema",
+  async (maHeThongRap, { rejectWithValue }) => {
+    try {
+      const result = await axios({
+        url: `https://movienew.cybersoft.edu.vn/api/QuanLyRap/LayThongTinCumRapTheoHeThong?maHeThongRap=${maHeThongRap}`,
+        method: "GET",
+        headers: {
+          TokenCybersoft:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA5MCIsIkhldEhhblN0cmluZyI6IjI5LzA1LzIwMjYiLCJIZXRIYW5UaW1lIjoiMTc4MDAxMjgwMDAwMCIsIm5iZiI6MTc1MzAzMDgwMCwiZXhwIjoxNzgwMTYwNDAwfQ.KkGRtLpEsgoM4M_TapjOZIzvAwbay3QvXIwwN8XUqWk",
+        },
+      });
+      return result.data.content;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+export const fetchTimeShow = createAsyncThunk(
+  "movie/fetchTimeShow",
+  async (maHeThongRap, { rejectWithValue }) => {
+    try {
+      const result = await axios({
+        url: `https://movienew.cybersoft.edu.vn/api/QuanLyRap/LayThongTinLichChieuHeThongRap?maHeThongRap=${maHeThongRap}&maNhom=GP02`,
+        method: "GET",
+        headers: {
+          TokenCybersoft:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA5MCIsIkhldEhhblN0cmluZyI6IjI5LzA1LzIwMjYiLCJIZXRIYW5UaW1lIjoiMTc4MDAxMjgwMDAwMCIsIm5iZiI6MTc1MzAzMDgwMCwiZXhwIjoxNzgwMTYwNDAwfQ.KkGRtLpEsgoM4M_TapjOZIzvAwbay3QvXIwwN8XUqWk",
+        },
+      });
+      return result.data.content;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+const movieSlice = createSlice({
+  name: "movieSlice",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
+    // MOVIE DETAIL
     builder.addCase(fetchMovieDetail.pending, (state) => {
       state.loading = true;
     });
-
     builder.addCase(fetchMovieDetail.fulfilled, (state, action) => {
       state.loading = false;
       state.dataDetail = action.payload;
     });
-
     builder.addCase(fetchMovieDetail.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    });
+
+    // CINEMA LIST
+    builder.addCase(fetchCinemaList.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchCinemaList.fulfilled, (state, action) => {
+      state.loading = false;
+      state.dataCinemaList = action.payload;
+    });
+    builder.addCase(fetchCinemaList.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    });
+
+    // EACH CINEMA
+    builder.addCase(fetchCinema.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchCinema.fulfilled, (state, action) => {
+      state.loading = false;
+      state.dataCinema = action.payload;
+    });
+    builder.addCase(fetchCinema.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    });
+
+    // TIME SHOW
+    builder.addCase(fetchTimeShow.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchTimeShow.fulfilled, (state, action) => {
+      state.loading = false;
+      state.dataTimeShow = action.payload;
+    });
+    builder.addCase(fetchTimeShow.rejected, (state, action) => {
       state.loading = false;
       state.error = action.payload;
     });
   },
 });
 
-export default movieDetailSlice.reducer;
+export default movieSlice.reducer;
