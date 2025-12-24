16f0d8ddafba748627a052e748eb5b4067a00994
kimcuong2025 <ttkc.contact@gmail.com>
2025-12-25 06:58:46 +0700

    KC updates movie (add movie, search movie)

---
+++ MovieTicketBookingProject/src/pages/AdminTemplate/Movies/slice.js
@@ -0,0 +1,39 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import api from "./../../../services/api";
+
+export const addMovie = createAsyncThunk(
+  "adminMovies/addMovie",
+  async (formData, { rejectWithValue }) => {
+    try {
+      const result = await api.post("QuanLyPhim/ThemPhimUploadHinh", formData);
+      alert("Add movie sucess!");
+      return result.data.content;
+    } catch (error) {
+      alert(error.response.data.content);
+      return rejectWithValue(error.response.data.content);
+    }
+  }
+);
+
+const moviesManageSlice = createSlice({
+  name: "moviesManageSlice",
+  initialState: {
+    loading: false,
+    error: null,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(addMovie.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(addMovie.fulfilled, (state) => {
+        state.loading = false;
+      })
+      .addCase(addMovie.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default moviesManageSlice.reducer;
